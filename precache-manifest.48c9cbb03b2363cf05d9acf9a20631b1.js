fb4c3d36d2c27a7a8bf2468fbbd6f45b3611e901
mohammad.manzoor8972@gmail.com <root@LKNDSS0NRG3QD.local>
2020-06-14 14:26:38 +0530

    Updates

---
+++ precache-manifest.48c9cbb03b2363cf05d9acf9a20631b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c44ab423edc4a0915e5f04431fc1f2",
+    "url": "/apertum/index.html"
+  },
+  {
+    "revision": "b4fa14c09388b708d3a0",
+    "url": "/apertum/static/js/2.10bf8bac.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/apertum/static/js/2.10bf8bac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f0ffbf7b34550af0f09",
+    "url": "/apertum/static/js/main.2f231093.chunk.js"
+  },
+  {
+    "revision": "d5d42387098e86e095ca",
+    "url": "/apertum/static/js/runtime-main.c3533460.js"
+  }
+]);
